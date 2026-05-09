d04f0292f17f6a24ae07efa501fcc5b811e07708
christian reyes <creyese23@gmail.com>
2026-05-09 09:49:49 -0500

    Actualizacion backend proyecto dentvision 09/05/2026 09:50

---
+++ src/main/java/com/sena/creyese/dentvision_backend_springboot/entity/Insumo.java
@@ -0,0 +1,51 @@
+package com.sena.creyese.dentvision_backend_springboot.entity;
+
+import com.sena.creyese.dentvision_backend_springboot.enums.InventarioEstado;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@SuppressWarnings("ALL")
+@Entity
+@Table(name = "insumos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Insumo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idInsumo;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 500)
+    private String descripcion;
+
+    @Column(name = "stock_minimo", nullable = false)
+    private Integer stockMinimo;
+
+    @Column(name = "unidad_medida", nullable = false, length = 20)
+    private String unidadMedida;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado", nullable = false, length = 20)
+    private InventarioEstado estado;
+
+    @CreationTimestamp
+    @Column(name = "fecha_creacion", nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @UpdateTimestamp
+    @Column(name = "fecha_actualizacion", nullable = false)
+    private LocalDateTime fechaActualizacion;
+
+    @Column(name = "fecha_eliminacion")
+    private LocalDateTime fechaEliminacion;
+}
